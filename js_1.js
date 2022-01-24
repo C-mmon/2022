61545fda746645990d368de67685fdbd2bc8b738
Aniket Kumar <aniketsinha101@gmail.com>
2022-01-24 23:48:16 +0530

    Create js_1.js

---
+++ js_1.js
@@ -0,0 +1,18 @@
+let text="Hello World";
+let index=4;
+let letter=text.charCodeAt(index)
+//writing literal 
+console.log(`the character code is ${letter}`);
+let char_at=text.charAt(1);
+
+
+//print the last character
+let last_char=text.charAt(text.length-1);
+console.log(last_char);
+//write a function to concat two string 
+let result=text.concat("","aniket","kumar");
+console.log(result);
+let text_store_booleans=text.endsWith("word");
+//similarly starts with 
+let char=String.fromCharCode(1,2,3,4,56);
+includes
